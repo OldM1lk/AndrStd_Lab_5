d624555a395d3c1275ef2dcfe118be67d13676c9
OldM1lk <MrlaimWin@gmail.com>
2024-10-28 17:27:38 +0700

    Add new branch and code for task1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.timber)
+    implementation(libs.gson)
+    implementation(libs.okhttp)
+    implementation(libs.glide)
+    implementation(libs.androidx.recyclerview)
 }
